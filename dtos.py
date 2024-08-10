7d3a902c2cbcf3fdc486c64def7b144d3e4abe7a
vodiylikbola <odilovmadiyor029@gmail.com>
2024-08-10 18:37:31 +0500

    dtos added

---
+++ dtos.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserCreate(BaseModel):
+    full_name: str
+    tg_id: int
+
+class UserUpdate(BaseModel):
+    id: int
+    full_name: Optional[str]
+    total_count: Optional[int]
+
+class UserResult(BaseModel):
+    full_name: str
+    total_count: int
+    share_link: str
+
+class LinkCreate(BaseModel):
+    name: str
+    url: str
+
+class LinkResult(BaseModel):
+    name: str
+    url: str
+
+class DarajaCreate(BaseModel):
+    name: str
+    daraja: int
+    started_at: int
+    limit: int
+
+class DarajaUpdate(BaseModel):
+    id: int
+    name: str
+    started_at: int
+    limit: int
+class DarajaResult(BaseModel):
+    name: str
+    daraja: int
+    started_at: int
+    limit: int
+
