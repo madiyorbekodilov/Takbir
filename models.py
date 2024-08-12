2f9b9b9e9df70920842bf3f4e59143e638a07a81
vodiylikbola <odilovmadiyor029@gmail.com>
2024-08-12 16:32:30 +0500

    bugs fixed

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:194853@localhost:5432/salom"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:194853@localhost:5432/hello"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
