b55445ce8f126ea30660155b80098eed716c4ced
vodiylikbola <odilovmadiyor029@gmail.com>
2024-08-13 10:51:34 +0500

    bugs fixed

--- dtos.py
+++ dtos.py
@@ -1,27 +1,33 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class UserCreate(BaseModel):
     full_name: str
     tg_id: int
+
 
 class UserUpdate(BaseModel):
     id: int
     full_name: Optional[str]
     total_count: Optional[int]
 
+
 class UserResult(BaseModel):
     full_name: str
     total_count: int
     share_link: str
 
+
 class LinkCreate(BaseModel):
     name: str
     url: str
 
+
 class LinkResult(BaseModel):
     name: str
     url: str
+
 
 class DarajaCreate(BaseModel):
     name: str
@@ -29,14 +35,21 @@ class DarajaCreate(BaseModel):
     started_at: int
     limit: int
 
+
 class DarajaUpdate(BaseModel):
     id: int
     name: str
     started_at: int
     limit: int
+
+
 class DarajaResult(BaseModel):
     name: str
     daraja: int
     started_at: int
     limit: int
 
+
+class FriendCreate(BaseModel):
+    user_id: int
+    friend_tg_id: int
