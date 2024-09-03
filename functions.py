4070b2d2f4881b48302e7629160ed752c2091f4e
vodiylikbola <odilovmadiyor029@gmail.com>
2024-09-03 14:26:45 +0500

    bugs fixed

--- functions.py
+++ functions.py
@@ -297,7 +297,7 @@ async def my_daraja(total_count: int, db):
     user_daraja = 1
     for daraja in db_daraja:
         if daraja.started_at <= total_count:
-            user_daraja += daraja.daraja
+            user_daraja += 1
             break
 
     return user_daraja
